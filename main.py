d3784d3d5d7b3302606f571f23ab53e46b0ae65d
ronnie <mohdamir7505@gmail.com>
2025-07-11 03:19:51 +0530

    added monetag auto ads and remoed hilltop ads

--- main.py
+++ main.py
@@ -351,4 +351,4 @@ async def advance_page(short_code: str, request: Request, db: Session = Depends(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5500)
